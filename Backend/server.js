d2eef2887edf05371ea15e98e0ef0e1abc6a9fe4
yassa98 <yasirudeshan3@gmail.com>
2021-08-17 13:24:39 +0530

    Post a comment api added

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express(); 
+const commentRoute = require("./routes/comment");
 require("dotenv").config();
 
 const PORT = process.env.PORT||8070;
@@ -26,6 +27,8 @@ const connection = mongoose.connection;
 connection.once("open", ()=>{
     console.log("Mongodb connection success!");
 });
+
+app.use("/api/comments", commentRoute);
 
 app.listen(PORT,()=>{
     console.log(`Server is up and running on port ${PORT}`)
