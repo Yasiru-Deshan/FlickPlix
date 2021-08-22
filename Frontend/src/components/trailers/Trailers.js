1444f3652dfdc60a57dab752cad3fac7f123ec2e
miyelandi <meeriumk@gmail.com>
2021-08-22 14:14:05 +0530

    pages handler commited

---
+++ Frontend/src/components/trailers/Trailers.js
@@ -0,0 +1,27 @@
+import React, {useContext} from 'react'
+import {GlobalState} from '../../../GlobalState'
+import ProductItem from '../utils/productItem/ProductItem'
+import Loading from '../utils/loading/Loading'
+
+
+function Trailers() {
+    const state = useContext(GlobalState)
+    const [trailers] = state.productsAPI.products
+   const [isAdmin] =state.userAPI.isAdmin
+ 
+    return ( 
+        <> 
+        <div className="trailers">
+            {
+                trailers.map(trailer =>{
+                    return <ProductItem key={trailer._id} trailer={trailer} 
+                    isAdmin = {isAdmin} />
+                })
+            }
+        </div>  
+        {trailers.length === 0 && <Loading />}
+        </>
+    )
+}
+
+export default Trailers
