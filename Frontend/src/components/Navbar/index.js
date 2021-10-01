cae2f4d4f7ce0636c548d1905672b10ed74d292e
yassa98 <yasirudeshan3@gmail.com>
2021-10-01 23:19:14 +0530

    Some changes made

--- Frontend/src/components/Navbar/index.js
+++ Frontend/src/components/Navbar/index.js
@@ -74,7 +74,7 @@ const Navbar = ({ toggle}) => {
                                     offset={-80}>Favorites</NavLinks>
                       </NavItem>
                       <NavItem>
-                          <NavLinks to="/login/admin"
+                          <NavLinks to="/browsemimi"
                                     smooth={true} 
                                     duration={500} 
                                     spy={true} 
